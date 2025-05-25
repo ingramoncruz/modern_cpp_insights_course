3aa6c8e37ae00ba633ce8134c3046643f3997fed
Ramon <ing.ramoncruz@gmail.com>
2025-05-25 11:35:17 -0700

    example 7 loaded

--- Day5/ex7.cpp
+++ Day5/ex7.cpp
@@ -4,4 +4,16 @@
 
 void printEverything() {std::cout << "null\n";}
 
-// template<typename T, typename... S> 
+template<typename T, typename... S> 
+void printEverything(T first, S... rest) {
+    std::cout << first << " ";
+    printEverything(rest...);
+}
+
+int main() {
+    printEverything(31.95f, 1, 2.5, "Hello", 'A', 42);
+    return 0;
+}
+
+// recursive function call, which happens again and again, at the end
+// when no input, it goes to the base case, which is printEverything()
